259976df112faf44d158df2728e1f83e80a67d3a
Christopher T Earley <jrekct@gmail.com>
2023-12-29 10:54:46 -0700

    Adding Orchestrator Object

---
+++ orchestartors/Orchestrator.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+
+class Orchestrator(ABC):
+    @abstractmethod
+    def conversation_with_data(self, request_body):
+        pass
+
+    @abstractmethod
+    def conversation_without_data(self, request_body):
+        pass
